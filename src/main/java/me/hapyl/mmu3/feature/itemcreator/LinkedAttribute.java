d9e203603c5bcc92ebd944e99da5c81c835229f6
hapyl <deojoff@gmail.com>
2024-11-08 04:13:28 +0500

    Update to 1.21.3

--- src/main/java/me/hapyl/mmu3/feature/itemcreator/LinkedAttribute.java
+++ src/main/java/me/hapyl/mmu3/feature/itemcreator/LinkedAttribute.java
@@ -6,23 +6,23 @@ import org.bukkit.attribute.Attribute;
 
 public enum LinkedAttribute {
 
-    MAX_HEALTH(Attribute.GENERIC_MAX_HEALTH, Material.APPLE, "Maximum health of an Entity."),
-    FOLLOW_RANGE(Attribute.GENERIC_FOLLOW_RANGE, Material.LEAD, "Range at which an Entity will follow others."),
-    KNOCKBACK_RESISTANCE(Attribute.GENERIC_KNOCKBACK_RESISTANCE, Material.NETHERITE_INGOT, "Resistance of an Entity to knockback."),
-    MOVEMENT_SPEED(Attribute.GENERIC_MOVEMENT_SPEED, Material.RABBIT_FOOT, "Movement speed of an Entity."),
+    MAX_HEALTH(Attribute.MAX_HEALTH, Material.APPLE, "Maximum health of an Entity."),
+    FOLLOW_RANGE(Attribute.FOLLOW_RANGE, Material.LEAD, "Range at which an Entity will follow others."),
+    KNOCKBACK_RESISTANCE(Attribute.KNOCKBACK_RESISTANCE, Material.NETHERITE_INGOT, "Resistance of an Entity to knockback."),
+    MOVEMENT_SPEED(Attribute.MOVEMENT_SPEED, Material.RABBIT_FOOT, "Movement speed of an Entity."),
     FLYING_SPEED(
-            Attribute.GENERIC_FLYING_SPEED,
+            Attribute.FLYING_SPEED,
             Material.ELYTRA,
             "Flying speed of an Entity. Only works for bees and parrots. (Unfortunately)"
     ),
-    ATTACK_DAMAGE(Attribute.GENERIC_ATTACK_DAMAGE, Material.IRON_SWORD, "Attack damage of an Entity."),
-    ATTACK_KNOCKBACK(Attribute.GENERIC_ATTACK_KNOCKBACK, Material.SLIME_BALL, "Attack knockback of an Entity."),
-    ATTACK_SPEED(Attribute.GENERIC_ATTACK_SPEED, Material.DIAMOND_HOE, "Attack speed of an Entity."),
-    ARMOR(Attribute.GENERIC_ARMOR, Material.IRON_CHESTPLATE, "Armor bonus of an Entity.S"),
-    ARMOR_TOUGHNESS(Attribute.GENERIC_ARMOR_TOUGHNESS, Material.IRON_BLOCK, "Armor durability bonus of an Entity."),
-    LUCK(Attribute.GENERIC_LUCK, Material.BIG_DRIPLEAF, "Luck bonus of an Entity."),
-    HORSE_JUMP_STRENGTH(Attribute.GENERIC_JUMP_STRENGTH, Material.LEATHER_HORSE_ARMOR, "Strength with which a horse will jump."),
-    ZOMBIE_SPAWN_REINFORCEMENTS(Attribute.ZOMBIE_SPAWN_REINFORCEMENTS, Material.ZOMBIE_HEAD, "Chance of a zombie to spawn reinforcements.");
+    ATTACK_DAMAGE(Attribute.ATTACK_DAMAGE, Material.IRON_SWORD, "Attack damage of an Entity."),
+    ATTACK_KNOCKBACK(Attribute.ATTACK_KNOCKBACK, Material.SLIME_BALL, "Attack knockback of an Entity."),
+    ATTACK_SPEED(Attribute.ATTACK_SPEED, Material.DIAMOND_HOE, "Attack speed of an Entity."),
+    ARMOR(Attribute.ARMOR, Material.IRON_CHESTPLATE, "Armor bonus of an Entity.S"),
+    ARMOR_TOUGHNESS(Attribute.ARMOR_TOUGHNESS, Material.IRON_BLOCK, "Armor durability bonus of an Entity."),
+    LUCK(Attribute.LUCK, Material.BIG_DRIPLEAF, "Luck bonus of an Entity."),
+    HORSE_JUMP_STRENGTH(Attribute.JUMP_STRENGTH, Material.LEATHER_HORSE_ARMOR, "Strength with which a horse will jump."),
+    ZOMBIE_SPAWN_REINFORCEMENTS(Attribute.SPAWN_REINFORCEMENTS, Material.ZOMBIE_HEAD, "Chance of a zombie to spawn reinforcements.");
 
     private final Attribute link;
     private final Material material;
